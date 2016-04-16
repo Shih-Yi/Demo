4e3660db00007feaacab58168b029b0584dc65a8
ShihYi <brvast@gmail.com>
2016-04-16 22:21:16 +0800

    add Event API update and delete

--- app/controllers/api_v1/events_controller.rb
+++ app/controllers/api_v1/events_controller.rb
@@ -20,4 +20,23 @@ class ApiV1::EventsController < ApiController
     end
   end
 
+  def update
+    @event = Event.find( params[:id] )
+
+    if @event.update( :name => params[:name],
+        :description => params[:description] )
+
+      render :json => { :id => @event.id }
+    else
+      render :json => { :message => "failed", :errors => @event.errors }, :status => 400
+    end
+  end
+
+  def destroy
+    @topic = Topic.find( params[:id] )
+    @topic.destroy
+
+    render :json => { :message => "OK" }
+  end
+
 end
