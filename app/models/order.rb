878f169620c7cc1c3795ec1c845cd6b600e7c7a4
ShihYi <brvast@gmail.com>
2016-04-17 23:34:38 +0800

    refactor model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,11 @@ class Order < ActiveRecord::Base
   has_many :line_items
   has_many :products, :through => :line_items
 
+
+  def clone_cart_line_items(cart)
+    cart.line_items.each do |line|
+      self.line_items.build( :product => line.product, :qty => line.qty )
+    end
+  end
+
 end
